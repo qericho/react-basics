08ab195d720331a0649d78da7f8da32e0038176b
echo <echstmr@gmail.com>
2025-09-15 23:34:13 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Accordion from "./Project_1/Accordion";
-import RandomColor from "./Project_2/RandomColor";
-import ImageSlider from "./Project_3/ImageSlider";
+import Accordion from "./project_1/Accordion";
+import RandomColor from "./project_2/RandomColor";
+import ImageSlider from "./project_3/ImageSlider";
 
 const App = () => {
   return (
