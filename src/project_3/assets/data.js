eca92135690c2ba7b42f4fc7933a088bf76a2b23
qericho <echstmr@gmail.com>
2025-09-15 23:28:53 +0800

    updated

---
+++ src/project_3/assets/data.js
@@ -0,0 +1,21 @@
+import Img_1 from "../assets/img/img-1.jpg";
+import Img_2 from "../assets/img/img-2.jpg";
+import Img_3 from "../assets/img/img-3.jpg";
+
+export const images = [
+  {
+    id: 1,
+    title: "image-1",
+    image: Img_1,
+  },
+  {
+    id: 2,
+    title: "image-2",
+    image: Img_2,
+  },
+  {
+    id: 3,
+    title: "image-2",
+    image: Img_3,
+  },
+];
